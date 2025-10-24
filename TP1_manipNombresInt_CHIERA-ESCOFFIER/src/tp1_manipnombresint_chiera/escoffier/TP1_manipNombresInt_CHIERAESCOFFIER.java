6189823fe5a7997bb3bc612a4ad9f977c49ce0a5
juliette.chiera-escoffier@epfedu.fr <julie@10.3.62.103>
2025-10-24 12:00:29 +0200

    ajout TP1 manip NombresInt

---
+++ TP1_manipNombresInt_CHIERA-ESCOFFIER/src/tp1_manipnombresint_chiera/escoffier/TP1_manipNombresInt_CHIERAESCOFFIER.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_chiera.escoffier;
+
+/**
+ * date 24/10
+ * @author juliette
+ * Saisie et manipulation de nombres
+ */
+public class TP1_manipNombresInt_CHIERAESCOFFIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
