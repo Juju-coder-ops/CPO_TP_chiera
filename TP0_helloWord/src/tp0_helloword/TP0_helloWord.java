a68283e4e3d3811a47d93cfaf8638ac9d73a3ecc
juliette.chiera-escoffier@epfedu.fr <julie@10.3.62.103>
2025-10-24 11:47:25 +0200

    ajout tp0 helloword

---
+++ TP0_helloWord/src/tp0_helloword/TP0_helloWord.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloword;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/*
+ * @author juliette
+ * @role
+ * @date 22/10/2025
+ */
+public class TP0_helloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ;
+        System.out.println( " Au revoir " ) ;
+        Scanner sc ; 
+        sc = new Scanner (System.in) ; 
+        System.out.println("Quel est votre prénom ?"); 
+        sc.nextLine(); 
+    }
+    
+}
