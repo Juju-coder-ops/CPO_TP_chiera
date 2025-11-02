8c5fb77da814981b12041f33ba4697caf4dcd850
juliette.chiera-escoffier@epfedu.fr <julie@10.3.62.103>
2025-11-02 23:28:57 +0100

    ajout TP1 GuessMyNumber

--- TP1_GuessMyNumber/src/tp1_guessmynumber/TP1_GuessMyNumber.java
+++ TP1_GuessMyNumber/src/tp1_guessmynumber/TP1_GuessMyNumber.java
@@ -1,20 +1,73 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TP1 – Guess My Number
+ * 02/11/2025
+ * Auteur : Juliette
  */
 package tp1_guessmynumber;
 
-/**
- *02/11/2025
- * @author julieette
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class TP1_GuessMyNumber {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Random generateurAleat = new Random();
+
+        System.out.println("Bienvenue dans Guess My Number !");
+        System.out.println("Choisissez un niveau de difficulté : facile, normal, difficile, cauchemar");
+        String difficulte = sc.nextLine().toLowerCase();
+
+        // Définition des paramètres selon la difficulté
+        int max = 100;
+        int maxCoups = Integer.MAX_VALUE; // par défaut illimité
+        boolean modeCauchemar = false;
+
+        switch (difficulte) {
+            case "facile" -> max = 50;             
+            case "normal" -> max = 100;
+            case "difficile" -> max = 200;         
+            case "cauchemar" -> {
+                max = 100;
+                modeCauchemar = true;
+            }
+            default -> System.out.println("Difficulté non reconnue, mode normal choisi.");
+        }
+
+        int nombreADeviner = generateurAleat.nextInt(max + 1);
+        int coups = 0;
+        boolean trouve = false;
+
+        System.out.println("Devinez un nombre entre 0 et " + max + " !");
+
+        while (!trouve && coups < maxCoups) {
+            System.out.print("Entrez votre proposition : ");
+            int proposition = sc.nextInt();
+            coups++;
+
+            String message;
+
+            if (proposition == nombreADeviner) {
+                message = "Gagné ! Vous avez trouvé le nombre en " + coups + " coups.";
+                trouve = true;
+            } else {
+                // Gestion mode cauchemar (30% de fausse indication)
+                boolean fausseIndication = modeCauchemar && generateurAleat.nextInt(100) < 30;
+
+                if ((proposition < nombreADeviner && !fausseIndication) || (proposition > nombreADeviner && fausseIndication)) {
+                    message = Math.abs(proposition - nombreADeviner) > max / 10 ? "Vraiment trop petit !" : "Trop petit";
+                } else {
+                    message = Math.abs(proposition - nombreADeviner) > max / 10 ? "Vraiment trop grand !" : "Trop grand";
+                }
+            }
+
+            System.out.println(message);
+        }
+
+        if (!trouve) {
+            System.out.println("Vous n'avez pas trouvé le nombre. C'était : " + nombreADeviner);
+        }
+
+        sc.close();
     }
-    
 }
