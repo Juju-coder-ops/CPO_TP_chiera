3aca81f163943328db14b6a578048e8a4d1da011
juliette.chiera-escoffier@epfedu.fr <julie@10.3.62.103>
2025-11-18 12:24:08 +0100

    ajout TP1 GuessMyNumber

--- TP2_Bieres_Chiera-Escoffier/src/tp2_bieres_chiera/escoffier/BouteilleBiere.java
+++ TP2_Bieres_Chiera-Escoffier/src/tp2_bieres_chiera/escoffier/BouteilleBiere.java
@@ -6,12 +6,35 @@ package tp2_bieres_chiera.escoffier;
  */
 public class BouteilleBiere {
     String nom;
-    double degreAlcool;
+    float degreAlcool;
     String brasserie;
     boolean ouverte;
 
     public void lireEtiquette() { 
         System.out.println("Bouteille de " + nom + " (" + degreAlcool + 
         " degrés)\nBrasserie : " + brasserie);
+    }
+    public BouteilleBiere(String Nom,float Degre, String Brasserie){
+        nom = Nom;
+        degreAlcool = Degre;
+        brasserie = Brasserie;
+        ouverte = false;
+    }
+        
+    public boolean Decapsuler(){
+        if (ouverte=true){
+           System.out.println("Erreur : bière déjà ouverte !");
+           return false;
+        }else {
+            return true;
+        }
+    }
+      
+    @Override
+    public String toString() {
+        String chaine_a_retourner = nom + " (" + degreAlcool + " degrés) - Ouverte ? ";
+        if (ouverte) chaine_a_retourner += "oui";
+        else chaine_a_retourner += "non";
+        return chaine_a_retourner;
     }
 }
