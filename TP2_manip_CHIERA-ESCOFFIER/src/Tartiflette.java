07b2f87693246cd5d45521a129b77f6f0f02434e
juliette.chiera-escoffier@epfedu.fr <julie@10.3.62.103>
2025-11-26 15:05:51 +0100

    ajout TP manip

---
+++ TP2_manip_CHIERA-ESCOFFIER/src/Tartiflette.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author julie
+ */
+public class Tartiflette {
+    public int nbCalories;
+    
+    public Tartiflette(int nbCalories){
+        this.nbCalories = nbCalories;
+    }
+}
