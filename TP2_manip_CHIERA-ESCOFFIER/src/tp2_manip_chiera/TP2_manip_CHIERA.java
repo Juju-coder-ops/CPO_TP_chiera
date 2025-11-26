a72ea2ef3086894fdf43e8b2716f323d2189840e
juliette.chiera-escoffier@epfedu.fr <julie@10.3.62.103>
2025-11-26 15:25:26 +0100

    ajout TP manip

--- TP2_manip_CHIERA-ESCOFFIER/src/tp2_manip_chiera/TP2_manip_CHIERA.java
+++ TP2_manip_CHIERA-ESCOFFIER/src/tp2_manip_chiera/TP2_manip_CHIERA.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tp2_manip_chiera.escoffier;
+package tp2_manip_chiera;
 
 /**
  *26/11/2025
  * @author juliette
  */
-public class TP2_manip_CHIERAESCOFFIER {
+public class TP2_manip_CHIERA {
 
     /**
      * @param args the command line arguments
