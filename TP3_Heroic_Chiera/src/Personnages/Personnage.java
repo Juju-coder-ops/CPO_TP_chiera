b4d7044c9885b35b5667ca5fec6bac720aa4f95c
juliette.chiera-escoffier@epfedu.fr <julie@10.3.62.103>
2025-11-26 23:25:50 +0100

    ajout TP heroic

--- TP3_Heroic_Chiera/src/Personnages/Personnage.java
+++ TP3_Heroic_Chiera/src/Personnages/Personnage.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Personnages;
+import java.util.ArrayList;
+import Armes.Arme;
 
 /**
  *
@@ -12,10 +14,15 @@ public abstract class Personnage {
 
     protected String nom;
     protected int niveauVie;
+  
+    protected ArrayList<Arme> inventaire;
+    protected Arme armeEnMain;
 
     public Personnage(String nom, int niveauVie) {
         this.nom = nom;
         this.niveauVie = niveauVie;
+        this.inventaire = new ArrayList<>();  // tableau dynamique vide au départ
+        this.armeEnMain = null; // aucune arme équipée au début
     }
 
     public String getNom() {
@@ -25,9 +32,40 @@ public abstract class Personnage {
     public int getNiveauVie() {
         return niveauVie;
     }
+    
+    // méthode pour ajouter une arme au personnage
+    public boolean ajouterArme(Arme a) {
+        if (inventaire.size() >= 5) {
+            System.out.println(nom + " ne peut pas avoir plus de 5 armes !");
+            return false;
+        }
+        inventaire.add(a);
+        System.out.println(a.getNom() + " ajoutée à l’inventaire de " + nom);
+        return true;
+    }
+    
+    // getter pour l’arme en main
+    public Arme getArmeEnMain() {
+        return armeEnMain;
+    }
+    // méthode pour s’équiper d’une arme via son NOM
+    public void equiperParNom(String nomArme) {
 
+        for (Arme a : inventaire) {
+            if (a.getNom().equalsIgnoreCase(nomArme)) {
+                armeEnMain = a;
+                System.out.println(nom + " s’équipe de " + a.getNom());
+                return;
+            }
+        }
+
+        System.out.println(nom + " ne possède pas d’arme appelée " + nomArme);
+    }
+    
     @Override
     public String toString() {
-        return nom + " (vie=" + niveauVie + ")";
+        String armeTxt = (armeEnMain == null ? "Aucune arme équipée" : armeEnMain.toString());
+        return nom + " (vie = " + niveauVie + ") | Arme en main : " + armeTxt + 
+        " | Inventaire : " + inventaire.size() + " arme(s)";
     }
 }
--- TP3_Heroic_Chiera/src/tp3_heroic_chiera/TP3_Heroic_Chiera.java
+++ TP3_Heroic_Chiera/src/tp3_heroic_chiera/TP3_Heroic_Chiera.java
@@ -4,6 +4,7 @@
  */
 package tp3_heroic_chiera;
 import Armes.*;
+import Personnages.*;
 /**
  *
  * @author julie
@@ -25,6 +26,41 @@ public class TP3_Heroic_Chiera {
         System.out.println(durandal);
         System.out.println(chene);
         System.out.println(charme);
+        
+        
+        Magicien gandalf = new Magicien("Gandalf", 65, true);
+        Magicien garcimore = new Magicien("Garcimore", 44, false);
+
+        Guerrier conan = new Guerrier("Conan", 78, false);
+        Guerrier lannister = new Guerrier("Lannister", 45, true);
+
+        System.out.println(gandalf);
+        System.out.println(garcimore);
+        System.out.println(conan);
+        System.out.println(lannister);
+        
+        // Je crée un magicien et un guerrier
+        Magicien gandalf2 = new Magicien("Gandalf", 65, true);
+        Guerrier conan2 = new Guerrier("Conan", 78, false);
+
+        // Je crée quelques armes
+        Epee excalibur2 = new Epee("Excalibur", 7, 5);
+        Epee durandal2 = new Epee("Durandal", 4, 7);
+        Baton chene2 = new Baton("Chêne", 4, 5);
+
+        // Ajout d’armes
+        gandalf.ajouterArme(chene);
+        gandalf.ajouterArme(excalibur);
+
+        conan.ajouterArme(durandal);
+
+        // Equipement
+        gandalf.equiperParNom("Chêne");
+        conan.equiperParNom("Durandal");
+
+        // Affichage final
+        System.out.println(gandalf);
+        System.out.println(conan);
     }
     
 }
