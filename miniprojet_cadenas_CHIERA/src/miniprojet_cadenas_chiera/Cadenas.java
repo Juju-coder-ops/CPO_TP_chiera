1aca062938810c4f5af241baca5159ee062e1180
juliette.chiera-escoffier@epfedu.fr <julie@10.3.62.103>
2025-12-08 15:20:39 +0100

    ajout TP Code

--- miniprojet_cadenas_CHIERA/src/miniprojet_cadenas_chiera/Cadenas.java
+++ miniprojet_cadenas_CHIERA/src/miniprojet_cadenas_chiera/Cadenas.java
@@ -60,5 +60,96 @@ public class Cadenas {
         // ⚠️ Décommenter pour tester :
         // System.out.println("SECRET = " + secret);
     }
+    
+    //EVALUER
+    public Resultat evaluer(String proposition) {
+
+    // 1) si la partie est déjà finie : on ne peut plus jouer
+    if (etat != Etat.EN_COURS) {
+        throw new IllegalStateException("La partie est terminée");
+    }
+
+    // 2) Vérifier longueur valide
+    if (proposition == null || proposition.length() != longueur) {
+        throw new IllegalArgumentException("Vous devez entrer " + longueur + " chiffres");
+    }
+
+    int exact = 0;
+    int tropHaut = 0;
+    int tropBas = 0;
+
+    // 3) Comparaison chiffre par chiffre
+    for (int i = 0; i < longueur; i++) {
+
+        int cProposition = Character.getNumericValue(proposition.charAt(i));
+        int cSecret = Character.getNumericValue(secret.charAt(i));
+
+        if (cProposition == cSecret) {
+            exact++;
+        } else if (cProposition > cSecret) {
+            tropHaut++;
+        } else {
+            tropBas++;
+        }
+    }
+
+    tentativeCourante++;
+
+    // 4) Mise à jour de l'état
+    if (exact == longueur) {
+        etat = Etat.GAGNE;
+    } else if (tentativeCourante >= maxTentatives) {
+        etat = Etat.PERDU;
+    }
+
+    // 5) On enregistre dans l'historique
+    historique.add(new Essai(proposition, exact, tropHaut, tropBas));
+
+    // 6) Et on renvoie le résultat
+    return new Resultat(proposition, exact, tropHaut, tropBas);
+}
+    //CLASS INTERNES 
+    public static class Resultat {
+        public final String proposition;
+        public final int exact;
+        public final int tropHaut;
+        public final int tropBas;
+
+        public Resultat(String p, int e, int h, int b) {
+            proposition = p;
+            exact = e;
+            tropHaut = h;
+            tropBas = b;
+        }
+    }
+    
+    
+    public static class Essai {
+        public final String proposition;
+        public final int exact;
+        public final int tropHaut;
+        public final int tropBas;
+
+        public Essai(String p, int e, int h, int b) {
+            proposition = p;
+            exact = e;
+            tropHaut = h;
+            tropBas = b;
+        }
+    }
+    
+    //GETTERS
+    
+    public Etat getEtat() {
+        return etat;
+    }
+
+    public int getTentativeCourante() {
+        return tentativeCourante;
+    }
+
+    public List<Essai> getHistorique() {
+        return historique;
+    }
 
 }
